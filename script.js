096abbc2857d2d4530ccc14b6e5072f7f9cfae4c
TatianaVolyntseva <tvolyntseva.exceedteam@gmail.com>
2021-04-02 18:09:44 +0300

    02.04.21

--- script.js
+++ script.js
@@ -13,56 +13,36 @@ let editFlagInputHowMuch = -1;
 let flagAllItemsChange = true;
 
 window.onload = function() {
-
-  let inputWhere = document.getElementById("inputWhere");
+  const inputWhere = document.getElementById("inputWhere");
   inputWhere.addEventListener("change", inputWhereAndDateGet);
-  
-  let inputHowMuch = document.getElementById("inputHowMuch");
+  const inputHowMuch = document.getElementById("inputHowMuch");
   inputHowMuch.addEventListener("change", inputHowMuchGet);
-  
-  let inputButton = document.getElementById("inputButton");
+  const inputButton = document.getElementById("inputButton");
   inputButton.addEventListener("click", addElementToList);
-
   containertotalSum = document.getElementById("containertotalSum");
   forDeleteTotal = document.getElementById("forDeleteTotal");
-
-  //создаю контейнер для всех OutPut'ов
-  wrapper = document.getElementById("wrapper");
-  containerForOutnput = document.createElement("div");
-  containerForOutnput.className = "containerForOutnput";
-  wrapper.appendChild(containerForOutnput);
-
-  //создаю нумерованный список ol
-  allItemsHere = document.createElement("ol");
-  allItemsHere.className = "allItemsHere";
-  containerForOutnput.appendChild(allItemsHere);
-
   render();
 }
 
-inputWhereAndDateGet = (e) =>{
-
+inputWhereAndDateGet = (e) => {
   inputWhereValue = e.target.value;
-
   let temp = new Date().toLocaleDateString();
   temp2 = temp.split(".");
   temp2 =temp2[2]+"-"+ temp2[1]+"-"+temp2[0];
   inputDateValue = temp2;
 }
 
-inputHowMuchGet = (e) =>{
+inputHowMuchGet = (e) => {
   inputHowMuchValue = e.target.value;
 }
 
-addElementToList = () =>{
- 
-  if (inputWhereValue && inputHowMuchValue && editFlag < 0) {
+addElementToList = () => {
+   if (inputWhereValue && inputHowMuchValue && editFlag < 0) {
     list.push({
       thing: inputWhereValue,
       money: inputHowMuchValue,
       date: inputDateValue
     });
-
     localStorage.setItem("list", JSON.stringify(list));  
     inputWhereValue = "";
     inputHowMuchValue = "";
@@ -82,182 +62,42 @@ addElementToList = () =>{
   render();
 }
 
-render = () => {
- 
-  while (allItemsHere.firstChild) {
-    allItemsHere.firstChild.remove();
-  }
-
-  total = 0;
-  list.map((item,index) => {
-    
-    //создаю экземпляр списка li
-    let listItem = document.createElement("li");
-    listItem.id = `listItem_${index}`;
-    listItem.className = "listItem";
-    allItemsHere.appendChild(listItem);
-
-    //создаю обертку для картинок //
-    let itemChange = document.createElement("div");
-    itemChange.className = "itemChange";
-
-    //////////////////////  itemWhere /////////////////////////
-    if((editFlag === index) || (editFlagInputWhere === index)){
-      //PEДАКТИРУЮ экземпляр itemWhere
-     let itemWhereEdit = document.createElement("textarea");
-     itemWhereEdit.id = `itemWhereEdit_${index}`;
-     itemWhereEdit.className = "itemWhereEdit";
-     itemWhereEdit.value = list[index].thing;
-     inputWhereValue = list[index].thing;
-     itemWhereEdit.setAttribute("rows", "3");
-     listItem.appendChild(itemWhereEdit);
-     itemWhereEdit.focus();
-     itemWhereEdit.addEventListener("change", function() {inputWhereValue = itemWhereEdit.value});  
-     if(flagAllItemsChange){
-        itemWhereEdit.addEventListener("blur", function() { list[index].thing = itemWhereEdit.value;  
-                                                            localStorage.setItem("list", JSON.stringify(list));
-                                                            editFlagInputWhere = -1;
-                                                            render();
-                                                          });
-      }
-
-    } else { //НE редактирую экземпляр itemWhere
-      let itemWhere = document.createElement("p");
-      itemWhere.id = `itemWhere_${index}`;
-      itemWhere.className = "itemWhere";
-      itemWhere.innerHTML = `${index+1}) ` + list[index].thing;
-      listItem.appendChild(itemWhere);
-      itemWhere.addEventListener("dblclick", function () {dbClickItemWhere(index)});
-    }
-
-    //////////////////////// itemDate /////////////////////////
-    if((editFlag === index) || (editFlagInputDate === index)){
-      //Если РЕДАКТИРУЮ itemDate
-      let itemDateEdit = document.createElement("input");
-      itemDateEdit.id = `itemDateEdit_${index}`;
-      itemDateEdit.className = "itemDateEdit";
-      itemDateEdit.type = "date";
-      itemDateEdit.value = list[index].date; 
-      listItem.appendChild(itemDateEdit);
-      itemDateEdit.focus();
-      itemDateEdit.addEventListener("change", function() {list[index].date = itemDateEdit.value});
-      if(flagAllItemsChange){
-        itemDateEdit.addEventListener("blur", function() {list[index].date = itemDateEdit.value;
-                                                            localStorage.setItem("list", JSON.stringify(list));
-                                                            editFlagInputDate = -1;
-                                                            render();
-                                                          });
-      } 
-
-    }else{//Если НЕ редактирую itemDate      
-      let itemDate = document.createElement("p");
-      itemDate.id = `itemDate_${index}`;
-      itemDate.className = "itemDate";       
-      itemDate.innerHTML = list[index].date;
-      listItem.appendChild(itemDate);
-      itemDate.addEventListener("dblclick", function () {dbClickItemDate(index)});
-      itemDate.addEventListener("doubletap", function () {dbClickItemDate(index)});
-    }
-
-    /////////////////////////// itemHowMuch //////////////////
-    if ((editFlag === index) || (editFlagInputHowMuch === index)) {
-      //Eсли РЕДАКТИРУЮ itemHowMuch
-      let itemHowMuchEdit = document.createElement("input");
-      itemHowMuchEdit.id = `itemHowMuchEdit_${index}`;
-      itemHowMuchEdit.type = "number";
-      itemHowMuchEdit.className = "itemHowMuchEdit";
-      itemHowMuchEdit.value = list[index].money;
-      inputHowMuchValue = list[index].money;
-      listItem.appendChild(itemHowMuchEdit);
-      itemHowMuchEdit.focus();
-      itemHowMuchEdit.addEventListener("change", function() {inputHowMuchValue = itemHowMuchEdit.value});
-      if(flagAllItemsChange){
-      itemHowMuchEdit.addEventListener("blur", function() {list[index].money = itemHowMuchEdit.value; 
-                                                              localStorage.setItem("list", JSON.stringify(list)); 
-                                                              editFlagInputHowMuch = -1;
-                                                              render();
-                                                            });
-      }
-    }
-    else{ //Если НЕ редактирую itemHowMuch
-      let itemHowMuch = document.createElement("p");
-      itemHowMuch.id = `itemHowMuch_${index}`;
-      itemHowMuch.className = "itemHowMuch";
-      itemHowMuch.innerHTML = list[index].money;
-      listItem.appendChild(itemHowMuch);
-      itemHowMuch.addEventListener("dblclick", function () {dbClickItemHowMuch(index)});
-      itemHowMuch.addEventListener("doubletap", function () {dbClickItemHowMuch(index)});
-    }
-
-    //////////////// Картинки ///////////
-    if (editFlag !== index){
-      //кладу в обертку для картинок картинку Edit
-      let imageEdit = document.createElement("img");
-      imageEdit.src = "img/edit.svg";
-      imageEdit.className = "imageEdit";
-      itemChange.appendChild(imageEdit);
-      imageEdit.addEventListener("click", function () {ImageEdit(index)});
-    } else {
-      //в обертку для картинок добавляю <img> Done
-      imgDone = document.createElement("img"); 
-      imgDone.src = "img/done.svg";
-      imgDone.className = "imageDone";
-      imgDone.addEventListener("click", function () {ImageDone(item, index)} );
-      itemChange.appendChild(imgDone);
-    }
-    //кладу обертку для картинок в конец каждого ли
-    listItem.appendChild(itemChange);
-    //кладу в обертку для картинок картинку Delete
-    let imageDelete = document.createElement("img");
-    imageDelete.src = "img/delete.svg";
-    imageDelete.className = "imageDelete";
-    itemChange.appendChild(imageDelete);
-    imageDelete.addEventListener("click", function () {ImageDelete(index)});
-    //расчитыаю всю потраченную сумму
-    total = total + Number(item.money);
-  }) 
-  //кладу всю потраченную сумму в  <p>
-  while (forDeleteTotal.firstChild) {
-    forDeleteTotal.firstChild.remove();
-  }
-  totalSum = document.createElement("p");
-  totalSum.id = "totalSum";
-  totalSum.innerHTML = "";
-  totalSum.innerHTML = total;
-  forDeleteTotal.appendChild(totalSum);
-} 
-
 ImageEdit = (index) => {
-  
   flagAllItemsChange = !flagAllItemsChange;
   editFlag = index;
   render();
 }
 
 ImageDone = (item) => {
-
   flagAllItemsChange = !flagAllItemsChange;
   item.thing = inputWhereValue;
   item.money = inputHowMuchValue;  
   localStorage.setItem("list", JSON.stringify(list));
- 
   editFlag = -1;
   editFlagInputWhere = -1;
   editFlagInputHowMuch = -1;
   editFlagInputDate = -1;
-
   inputWhereValue = "";
   inputHowMuchValue = "";
   inputWhere.value = "";
   inputHowMuch.value = "";
-
   render();  
 }
 
-ImageDelete = (index) => {
-  list.splice(index,1);
+ImageDelete = (index, itemWhereEdit) => {
+ if(flagAllItemsChange){
+   list.splice(index,1);
   localStorage.setItem("list", JSON.stringify(list));
   render();
+ } else {
+   console.log("sd");
+   flagAllItemsChange = !flagAllItemsChange;
+   editFlag = -1;
+   editFlagInputWhere = -1;
+   editFlagInputHowMuch = -1;
+   editFlagInputDate = -1;
+  render();
+ }
 }
 
 dbClickItemWhere = (index) => {
@@ -274,3 +114,133 @@ dbClickItemHowMuch = (index) => {
   editFlagInputHowMuch = index;
   render();
 }
+
+blurOnitemWhereEdit = (index, itemWhereEdit) => {
+  if(flagAllItemsChange){
+    list[index].thing = itemWhereEdit.value;  
+    localStorage.setItem("list", JSON.stringify(list));
+    editFlagInputWhere = -1;
+    inputWhereValue = "";
+    render();
+  }
+}
+
+blurOnitemDateEdit = (index, itemDateEdit) => {
+  if(flagAllItemsChange){
+    list[index].date = itemDateEdit.value;
+    localStorage.setItem("list", JSON.stringify(list));
+    editFlagInputDate = -1;
+    render();
+  }
+}
+
+blurOnitemHowMuchEdit = (index, itemHowMuchEdit) => {
+  if(flagAllItemsChange){
+    list[index].money = itemHowMuchEdit.value; 
+    localStorage.setItem("list", JSON.stringify(list)); 
+    editFlagInputHowMuch = -1;
+    inputHowMuchValue = "";
+    render();
+  }
+}
+
+render = () => {
+  const allItemsHere = document.getElementById('allItemsHere');
+  while (allItemsHere.firstChild) {
+    allItemsHere.firstChild.remove();
+  }
+  total = 0;
+  list.map((item,index) => { 
+    let listItem = document.createElement("li"); //create li
+    listItem.id = `listItem_${index}`;
+    listItem.className = "listItem";  
+    let itemChange = document.createElement("div");//create wrapper for picture
+    itemChange.className = "itemChange";
+    allItemsHere.appendChild(listItem);
+    //////////////////////  itemWhere /////////////////////////
+    if((editFlag === index) || (editFlagInputWhere === index)){
+      let itemWhereEdit = document.createElement("textarea"); //EDIT itemWhere
+      itemWhereEdit.id = `itemWhereEdit_${index}`;
+      itemWhereEdit.className = "itemWhereEdit";
+      itemWhereEdit.value = list[index].thing;
+      inputWhereValue = list[index].thing;
+      itemWhereEdit.setAttribute("rows", "3");
+      listItem.appendChild(itemWhereEdit);
+      itemWhereEdit.focus();
+      itemWhereEdit.addEventListener("change", () => {inputWhereValue = itemWhereEdit.value});  
+      itemWhereEdit.addEventListener("blur", () => blurOnitemWhereEdit(index, itemWhereEdit));
+    } else { 
+      let itemWhere = document.createElement("p");//DON'T edit itemWhere
+      itemWhere.id = `itemWhere_${index}`;
+      itemWhere.className = "itemWhere";
+      itemWhere.innerHTML = `${index+1}) ` + list[index].thing;
+      listItem.appendChild(itemWhere);
+      itemWhere.addEventListener("dblclick", () => {dbClickItemWhere(index)});
+    }
+    //////////////////////// itemDate /////////////////////////
+    if((editFlag === index) || (editFlagInputDate === index)){
+      let itemDateEdit = document.createElement("input");// EDIT itemDate
+      itemDateEdit.id = `itemDateEdit_${index}`;
+      itemDateEdit.className = "itemDateEdit";
+      itemDateEdit.type = "date";
+      itemDateEdit.value = list[index].date; 
+      listItem.appendChild(itemDateEdit);
+      itemDateEdit.focus();
+      itemDateEdit.addEventListener("change", () => {list[index].date = itemDateEdit.value});
+      itemDateEdit.addEventListener("blur", () => blurOnitemDateEdit(index, itemDateEdit));
+    } else {   
+      let itemDate = document.createElement("p");//If DON't itemDate   
+      itemDate.id = `itemDate_${index}`;
+      itemDate.className = "itemDate";       
+      itemDate.innerHTML = list[index].date;
+      listItem.appendChild(itemDate);
+      itemDate.addEventListener("dblclick", () => {dbClickItemDate(index)});
+      itemDate.addEventListener("doubletap", () => {dbClickItemDate(index)});
+    }
+    /////////////////////////// itemHowMuch //////////////////
+    if ((editFlag === index) || (editFlagInputHowMuch === index)) {
+      let itemHowMuchEdit = document.createElement("input"); // EDIT itemHowMuch
+      itemHowMuchEdit.id = `itemHowMuchEdit_${index}`;
+      itemHowMuchEdit.type = "number";
+      itemHowMuchEdit.className = "itemHowMuchEdit";
+      itemHowMuchEdit.value = list[index].money;
+      inputHowMuchValue = list[index].money;
+      listItem.appendChild(itemHowMuchEdit);
+      itemHowMuchEdit.focus();
+      itemHowMuchEdit.addEventListener("change",() => {inputHowMuchValue = itemHowMuchEdit.value});
+      itemHowMuchEdit.addEventListener("blur", () => blurOnitemHowMuchEdit(index, itemHowMuchEdit));
+    } else {
+      let itemHowMuch = document.createElement("p"); //if DON't  edit itemHowMuch
+      itemHowMuch.id = `itemHowMuch_${index}`;
+      itemHowMuch.className = "itemHowMuch";
+      itemHowMuch.innerHTML = list[index].money;
+      listItem.appendChild(itemHowMuch);
+      itemHowMuch.addEventListener("dblclick", () => {dbClickItemHowMuch(index)});
+      itemHowMuch.addEventListener("doubletap", () => {dbClickItemHowMuch(index)});
+    }
+    //////////////// Peactures ///////////
+    if (editFlag !== index){
+      let imageEdit = document.createElement("img");//кладу в обертку для картинок картинку Edit
+      imageEdit.src = "img/edit.svg";
+      imageEdit.className = "imageEdit";
+      itemChange.appendChild(imageEdit);
+      imageEdit.addEventListener("click", () => {ImageEdit(index)});
+    } else {
+      imgDone = document.createElement("img"); //в обертку для картинок добавляю <img> Done
+      imgDone.src = "img/done.svg";
+      imgDone.className = "imageDone";
+      imgDone.addEventListener("click", () => {ImageDone(item, index)} );
+      itemChange.appendChild(imgDone);
+    }
+    listItem.appendChild(itemChange);//кладу обертку для картинок в конец каждого ли
+    let imageDelete = document.createElement("img");//кладу в обертку для картинок картинку Delete
+    imageDelete.src = "img/delete.svg";
+    imageDelete.className = "imageDelete";
+    itemChange.appendChild(imageDelete);
+    imageDelete.addEventListener("click", () => {ImageDelete(index)});
+
+    total = total + Number(item.money); //расчитыаю всю потраченную сумму
+  })  
+  const ptotal = document.getElementById("total");//кладу всю потраченную сумму в  <p>
+  ptotal.innerText = `${total}  p.`;
+} 
